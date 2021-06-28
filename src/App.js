02242c29e892c5a3af4c2b42263800cb24a5b084
Bharathi <krangaraj@KONICAFR.LOCAL>
2021-06-28 14:24:26 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+import 'bootstrap';
+import 'react-bootstrap';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Login from './components/Login'
+import UserList from './components/UserList'
+import UserProfile from './components/UserProfile'
+import PageNotFound from './components/PageNotFound'
+
+const UserContext = React.createContext('');
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            user: " "
+        }
+    }
+    setUser = (user) => {
+        this.setState({ user })
+        // - La fonction `setUser` sera passée en props de la `Route` `Login` afin de l'utiliser plus tard.
+    }
+
+    render() {
+        return (
+            <UserContext.Provider value="dark">
+            <BrowserRouter>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <ul className="navbar-nav">
+                        <li className="nav-item" onClick={this.test}><Link className="nav-link" to="/">Home</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/Login">Login</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/UserList">UserList</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/UserProfile">UserProfile</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/PageNotFound">PageNotFound</Link></li>
+                    </ul>
+                </nav>
+                
+                    <Switch>
+                        <Route path="/PageNotFound" component={PageNotFound} />
+                        <Route exact path="/UserProfile" component={UserProfile} />
+                        <Route path="/UserList" component={UserList} />
+                        <Route path="/Login" component={Login} />
+                        <Route path="/"><Login /></Route>
+                    </Switch>
+                
+
+            </BrowserRouter>
+            </UserContext.Provider>
+        )
+    }
 }
 
-export default App;
+
+export default App
